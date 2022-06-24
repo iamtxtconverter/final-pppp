5604dc20a9e04b05cef71531568acd71b2fb7bfd
iamtxtconverter <95140815+iamtxtconverter@users.noreply.github.com>
2022-06-24 17:44:21 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$con=mysqli_connect("localhost","root","","dbdanceacademy");
-if(mysqli_connect_error($con))
-{
-  echo mysqli_error($con);
+// $con=mysqli_connect("localhost","root","","dbdanceacademy");
+// if(mysqli_connect_error($con))
+// {
+//   echo mysqli_error($con);
  
-}
+// }
 ?>
